e5d45495c6f52bf302c4c782ef798eeaa2c71978
yahir140306 <joseyahirhernandez11@gmail.com>
2025-09-17 11:15:19 -0600

    Refactor AlmacenarNumeros and PrincipalArray classes to enhance functionality and improve user prompts

--- java/AlmacenarNumeros.java
+++ java/AlmacenarNumeros.java
@@ -1,61 +1,74 @@
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class AlmacenarNumeros {
 
-    // public static void mostrarArregloAlReves(int[] arr) {
-    //     if (arr[0] == 0 && Arrays.stream(arr).sum() == 0) {
-    //         System.out.println("El arreglo está vacío. Por favor, llénelo primero (Opción 1).");
-    //         return;
-    //     }
-    //     System.out.println("Arreglo al revés:");
-    //     for (int i = arr.length - 1; i >= 0; i--) {
-    //         System.out.println(arr[i]);
-    //     }
-    // }
     public static void llenarArreglo(int[] arr, Scanner sc) {
         System.out.println("Ingrese 6 números enteros:");
+        System.out.println("\n");
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
         }
-        System.out.println("Arreglo llenado correctamente.");
+        System.out.println("\nArreglo llenado correctamente.");
     }
 
     public static void mostrarNumeroMayor(int[] arr) {
-        if (arr[0] == 0 && Arrays.stream(arr).sum() == 0) {
-            System.out.println("El arreglo está vacío. Por favor, llénelo primero (Opción 1).");
-            return;
-        }
-
         int maximo = arr[0];
-        int posicion = 0;
+        // int posicion = 0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > maximo) {
                 maximo = arr[i];
-                posicion = i;
+                // posicion = i;
             }
         }
         System.out.println("El número mayor es: " + maximo);
-        System.out.println("Se encuentra en la posición (índice) " + posicion);
     }
 
     public static void mostrarNumeroMenor(int[] arr) {
-        if (arr[0] == 0 && Arrays.stream(arr).sum() == 0) {
-            System.out.println("El arreglo está vacío. Por favor, llénelo primero (Opción 1).");
-            return;
-        }
-
         int minimo = arr[0];
-        int posicion = 0;
+        // int posicion = 0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < minimo) {
                 minimo = arr[i];
-                posicion = i;
+                // posicion = i;
             }
         }
         System.out.println("El número menor es: " + minimo);
-        System.out.println("Se encuentra en la posición (índice) " + posicion);
+    }
+
+    /////////////////////////////////////
+    
+    public static void llenarArreglo2(int[] arr, Scanner sc) {
+        System.out.println("Ingrese 5 números enteros:");
+        System.out.println("\n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("\nArreglo llenado correctamente.");
+    }
+
+    public static void ParImpar(int[] arr) {
+        int suma = 0;
+        // int pares = arr[0];
+        int mutiplicacion = 1;
+        int countPar = 0;
+        int countImpar = 0;
+        // int posicion = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                suma += arr[i];
+                countPar += 1;
+                // posicion = i;
+            }
+            // impares
+            if (arr[i] % 2 != 0) {
+                mutiplicacion *= arr[i];
+                countImpar += 1;
+            }
+        }
+        System.out.println(countPar + " pares y la suma de los pares es: " + suma);
+        System.out.println(countImpar + " impares y la multiplicación de los impares es: " + mutiplicacion);
     }
 }
--- java/PrincipalArray.java
+++ java/PrincipalArray.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class PrincipalArray {
 
-    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         // ArrayNumeros obj = new ArrayNumeros();
         // obj.Numeros[0] = 32;
@@ -14,45 +13,39 @@ public class PrincipalArray {
 
         // obj.llenarArrayPalabra();
         // obj.palabras();
-        // @SuppressWarnings("unused")
         AlmacenarNumeros objAlmacenarNumeros = new AlmacenarNumeros();
 
-        int[] numeros = new int[6];
+        int[] numeros = new int[5];
         Scanner scanner = new Scanner(System.in);
-        int opcion;
+        // int opcion;
+        // do {
+        //     System.out.println("\n--- Menú de Opciones ---");
+        //     System.out.println("1. Llenar arreglo");
+        //     System.out.println("2. Mostrar el número mayor");
+        //     System.out.println("3. Mostrar el número menor");
+        //     System.out.println("5. Salir");
+        //     System.out.print("Seleccione una opción: ");
+        //     opcion = scanner.nextInt();
+        //     switch (opcion) {
+        //         case 1:
+        //             objAlmacenarNumeros.llenarArreglo(numeros, scanner);
+        //             break;
+        //         case 2:
+        //             objAlmacenarNumeros.mostrarNumeroMayor(numeros);
+        //             break;
+        //         case 3:
+        //             objAlmacenarNumeros.mostrarNumeroMenor(numeros);
+        //             break;
+        //         case 5:
+        //             System.out.println("Saliendo de la aplicación. ¡Hasta luego!");
+        //             break;
+        //         default:
+        //             System.out.println("Opción no válida. Por favor, intente de nuevo.");
+        //     }
+        // } while (opcion != 5);
+        // scanner.close();
 
-        do {
-            System.out.println("\n--- Menú de Opciones ---");
-            System.out.println("1. Llenar arreglo");
-            System.out.println("2. Mostrar el número mayor");
-            System.out.println("3. Mostrar el número menor");
-            // System.out.println("4. Mostrar arreglo al revés");
-            System.out.println("5. Salir");
-            System.out.print("Seleccione una opción: ");
-            opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    objAlmacenarNumeros.llenarArreglo(numeros, scanner);
-                    break;
-                case 2:
-                    objAlmacenarNumeros.mostrarNumeroMayor(numeros);
-                    break;
-                case 3:
-                    objAlmacenarNumeros.mostrarNumeroMenor(numeros);
-                    break;
-                // case 4:
-                //     objAlmacenarNumeros.mostrarArregloAlReves(numeros);
-                //     break;
-                case 5:
-                    System.out.println("Saliendo de la aplicación. ¡Hasta luego!");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor, intente de nuevo.");
-            }
-        } while (opcion != 5);
-
-        scanner.close();
-
+        objAlmacenarNumeros.llenarArreglo2(numeros, scanner);
+        objAlmacenarNumeros.ParImpar(numeros);
     }
 }
