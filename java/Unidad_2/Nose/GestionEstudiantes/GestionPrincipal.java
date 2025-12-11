6d24f525a4a1171ad9856b5df65483ed6deac720
yahir140306 <joseyahirhernandez11@gmail.com>
2025-12-10 21:40:22 -0600

    Refactor GestionPrincipal to use Burbuja_Estudiante class for student management

--- java/Unidad_2/Nose/GestionEstudiantes/GestionPrincipal.java
+++ java/Unidad_2/Nose/GestionEstudiantes/GestionPrincipal.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GestionPrincipal {
-    private static List<Estudiante> listaEstudiantes = new ArrayList<>();
+    private static List<Burbuja_Estudiante> listaEstudiantes = new ArrayList<>();
 
     public static void main(String[] args) {
-        listaEstudiantes.add(new Estudiante("ABC001", "Juanchis", 9));
-        listaEstudiantes.add(new Estudiante("ABC002", "Paco", 8.5));
-        listaEstudiantes.add(new Estudiante("ABC003", "Panchito", 7));
+        listaEstudiantes.add(new Burbuja_Estudiante("ABC001", "Juanchis", 9));
+        listaEstudiantes.add(new Burbuja_Estudiante("ABC002", "Paco", 8.5));
+        listaEstudiantes.add(new Burbuja_Estudiante("ABC003", "Panchito", 7));
 
         Scanner scanner = new Scanner(System.in);
         String opcion;
@@ -43,7 +43,7 @@ public class GestionPrincipal {
                     System.out.println("\nIngrese calificacion:");
                     double cal = scanner.nextDouble();
 
-                    listaEstudiantes.add(new Estudiante(matricula, nombre, cal));
+                    listaEstudiantes.add(new Burbuja_Estudiante(matricula, nombre, cal));
                     System.out.println("Estudiante agregado correctamente.");
 
                     break;
@@ -52,7 +52,7 @@ public class GestionPrincipal {
                     System.out.println("\nIngrese la matricula a consultar:");
                     String matriculaConsulta = scanner.next();
 
-                    Estudiante encontrado = buscarPorMatricula(matriculaConsulta);
+                    Burbuja_Estudiante encontrado = buscarPorMatricula(matriculaConsulta);
 
                     if (encontrado != null) {
                         System.out.println("\nEstudiante encontrado");
@@ -66,7 +66,7 @@ public class GestionPrincipal {
                     System.out.println("\nIngrese la matricula a actualizar");
                     String matriculaActualizar = scanner.next();
 
-                    Estudiante aActualizar = buscarPorMatricula(matriculaActualizar);
+                    Burbuja_Estudiante aActualizar = buscarPorMatricula(matriculaActualizar);
 
                     if (aActualizar != null) {
                         System.out.println("\nCalificacion actual" + aActualizar.getCalificacion());
@@ -85,7 +85,7 @@ public class GestionPrincipal {
                     System.out.println("\nIngrese la matricula del estudiante: ");
                     String matriculaDelet = scanner.next();
 
-                    Estudiante aElminar = buscarPorMatricula(matriculaDelet);
+                    Burbuja_Estudiante aElminar = buscarPorMatricula(matriculaDelet);
 
                     if (aElminar != null) {
                         listaEstudiantes.remove(aElminar);
@@ -102,7 +102,7 @@ public class GestionPrincipal {
 
                     double sumaCalificaciones = 0;
 
-                    for (Estudiante estudiante : listaEstudiantes) {
+                    for (Burbuja_Estudiante estudiante : listaEstudiantes) {
                         sumaCalificaciones += estudiante.getCalificacion();
                     }
 
@@ -116,7 +116,7 @@ public class GestionPrincipal {
                         System.out.println("Lista de estudiantes vacia");
                     }
 
-                    for (Estudiante estudiante : listaEstudiantes) {
+                    for (Burbuja_Estudiante estudiante : listaEstudiantes) {
                         System.out.println("\n" + estudiante);
                     }
                     break;
@@ -129,8 +129,8 @@ public class GestionPrincipal {
         scanner.close();
     }
 
-    private static Estudiante buscarPorMatricula(String matricula) {
-        for (Estudiante estudiante : listaEstudiantes) {
+    private static Burbuja_Estudiante buscarPorMatricula(String matricula) {
+        for (Burbuja_Estudiante estudiante : listaEstudiantes) {
             if (estudiante.getMatricula().equals(matricula)) {
                 return estudiante;
             }
